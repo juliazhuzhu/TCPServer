f5fca475fb3aa6a4563e97e3c4cf15d42d17edb3
xiaoye <3144604@qq.com>
2021-12-19 20:53:41 +0800

    first commit

---
+++ epoll_server.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define PORT 8111
+#define MESSAGE_SIZE 1024
+#define MAX_EVENTS 20
+#define TIMEOUT 500
+
+int main() {
+//    std::cout << "Hello, World!" << std::endl;
+    int socket_fd = -1;
+    int accept_fd = -1;
+    int ret = -1;
+    int flags = 1;
+    int backlog = 10;
+
+    char in_buf[MESSAGE_SIZE] = {0,};
+    struct sockaddr_in local_addr, remote_addr;
+    int epoll_fd;
+    struct epoll_event ev, events[MAX_EVENTS];
+    int event_number;
+
+
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        std::cout << "create socket error" << std::endl;
+        exit(1);
+    }
+
+    flags = fcntl(socket_fd, F_GETFL, 0);
+    fcntl(socket_fd, F_SETFL, flags | O_NONBLOCK);
+
+    ret = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &flags, sizeof(flags));
+    if (ret == -1) {
+        std::cout << "setsocket error" << std::endl;
+    }
+
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(PORT);
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(local_addr.sin_zero), 8);
+
+    ret = bind(socket_fd, (struct sockaddr *) &local_addr, sizeof(struct sockaddr_in));
+    if (ret == -1) {
+        std::cout << "bind error" << std::endl;
+        exit(1);
+    }
+
+    ret = listen(socket_fd, backlog);
+    if (ret == -1) {
+        std::cout << "listen error" << std::endl;
+        exit(1);
+    }
+
+    epoll_fd = epoll_create(256);
+
+    ev.events = EPOLLIN;
+    ev.data.fd = socket_fd;
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &ev);
+
+    for (;;) {
+        event_number = epoll_wait(epoll_fd, events, MAX_EVENTS, TIMEOUT);
+        for (int i = 0; i < event_number; i++) {
+            if (events[i].data.fd == socket_fd) {
+                socklen_t addr_len = sizeof(struct sockaddr_in);
+                accept_fd = accept(socket_fd, (struct sockaddr *) &remote_addr, &addr_len);
+
+                flags = fcntl(accept_fd, F_GETFL, 0);
+                fcntl(accept_fd, F_SETFL, flags | O_NONBLOCK);
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = accept_fd;
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, accept_fd, &ev);
+            } else if (events[i].events & EPOLLIN) {
+                do {
+                    memset(in_buf, 0, MESSAGE_SIZE);
+                    ret = recv(events[i].data.fd, (void *) in_buf, MESSAGE_SIZE, 0);
+                    if (ret == 0) {
+                        close(events[i].data.fd);
+                    }
+
+                    if (ret == MESSAGE_SIZE) {
+                        std::cout << "" << std::endl;
+                    }
+                } while (ret < 0 && errno == EINTR);//这个操作被中断
+
+                if (ret < 0) {
+                    switch (errno) {
+                        //没有数据了
+                        case EAGAIN:
+                            break;
+                        default:
+                            break;
+
+
+                    }
+                }
+
+                if (ret > 0) {
+                    std::cout << "receive message:" << in_buf << std::endl;
+                    send(events[i].data.fd, (void *) in_buf, MESSAGE_SIZE, 0);
+                }
+            }
+        }
+
+
+    }
+
+    std::cout << "quit server..." << std::endl;
+    close(socket_fd);
+
+    return 0;
+
+}
---
+++ tcp_client.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+// accept
+#include <sys/socket.h>
+// connect
+#include <sys/types.h>
+//close
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+
+//使用宏，定义一个端口
+#define PORT 8111
+//sendbuf length
+#define MESSAGE_LEN 1024
+
+int main(int argc, char const *argv[])
+{
+    int ret = -1;
+    // define this socket param use to send and receive data
+    int socket_fd;
+
+    struct sockaddr_in serveraddr;
+    //set sendbuf array is empty,this is output
+    char sendbuf[MESSAGE_LEN] = {
+            0,
+    };
+    //set receive buffer, this is input
+    char recvbuf[MESSAGE_LEN] = {
+            0,
+    };
+
+    /// Step1: 创建Socket //
+    //PF_INET         Internet version 4 protocols,
+    //SOCK_STREAM TCP
+    //SOCK_DGRAM UDP
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1)
+    {
+        std::cout << "Failed to create socket!" << std::endl;
+        exit(-1);
+    }
+
+    /// Step2: 连接 //
+    // int connect(int socket,
+    //             const struct sockaddr *address, //要传一个地址
+    //             socklen_t address_len);
+
+//    serveraddr.sin_family = AF_INET;
+//    serveraddr.sin_port = PORT;
+//    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //inet_addr 将字符串转为数字
+//    ret = connect(socket_fd,
+//                  (struct sockaddr *)&serveraddr, //是一个地址
+//                  sizeof(struct sockaddr));
+//    if (ret < 0)
+//    {
+//        //connect error
+//        std::cout << "Failed to connect !" << std::endl;
+//        exit(1);
+//    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr,0,sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    char server_ip[] = "127.0.0.1";
+    if( inet_pton(AF_INET,server_ip,&server_addr.sin_addr) <=0 ) {
+        printf("inet_pton error for %s\n",server_ip);
+        exit(0);
+    }
+
+    if( connect(socket_fd,(struct sockaddr*)&server_addr,sizeof(server_addr))<0) {
+        printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
+        exit(0);
+    }
+
+
+
+    // send message from client
+    while (1)
+    {
+        // Send message to server///
+        //the first is 0,but the second maybe isn't 0,
+        //so need set 0 at this.
+        memset(sendbuf, 0, MESSAGE_LEN);
+        //get input data from terminal
+        fgets(sendbuf,MESSAGE_LEN-1, stdin);
+
+        // while exit if input "quit"
+        if (strcmp(sendbuf, "quit") == 0)
+        {
+            break;
+        }
+
+        // send information by terminal
+        ret = send(socket_fd, sendbuf, strlen(sendbuf), 0);
+        if (ret <= 0)
+        {
+            //send message to server is error
+            std::cout << "Failed to send data!" << std::endl;
+            break;
+        }
+
+        // receive message form server///
+        ret = recv(socket_fd, recvbuf, MESSAGE_LEN, 0);
+        // how to know the message is end? The zero at the end
+        recvbuf[ret] = '\0';
+        std::cout << "recv:" << recvbuf << std::endl;
+    }
+    close(socket_fd);
+    return 0;
+}
